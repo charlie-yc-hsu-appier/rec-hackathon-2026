9f6fc512af61df016ca3a763e4c439244e806b0c
Yvonne Feng <yvonne.feng@appier.com>
2025-08-20 16:54:59 +0800

    feat: setup controller skeleton

---
+++ internal/controller/recommender.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+type VendorController struct {
+	// TODO
+}
+
+// TODO: add swagger documentation
+func NewVendorController() *VendorController {
+	return &VendorController{}
+}
+
+type RecommendQuery struct {
+	VendorKey string `form:"vendor_key" binding:"required"`
+	UserID    string `form:"user_id" binding:"required"`
+	ClickID   string `form:"click_id"`
+	ImgWidth  int    `form:"image_width" binding:"required"`
+	ImgHeight int    `form:"image_height" binding:"required"`
+}
+
+func (c *VendorController) Recommend(ctx *gin.Context) {
+	var req RecommendQuery
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		log.WithContext(ctx).WithError(err).Errorf("fail to bind query parameter, uri: %s", ctx.Request.RequestURI)
+		handleBadRequest(ctx, err)
+		return
+	}
+
+	// TODO:
+	// response, err := vendorClient.GetRecommendationItems(ctx.Request.Context(), serviceReq)
+
+	ctx.JSON(http.StatusOK, "message: success")
+}
