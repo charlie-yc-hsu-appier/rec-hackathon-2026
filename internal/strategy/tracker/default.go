9aad0b515d2cc97f68507c848ec1b69a493596f7
Yvonne Feng <yvonne.feng@appier.com>
2025-08-26 18:13:07 +0800

    fix: click id encoding

--- internal/strategy/tracker/default.go
+++ internal/strategy/tracker/default.go
@@ -10,7 +10,7 @@ type Default struct{}
 func (s *Default) GenerateTrackingURL(params Params) string {
 	trackingURL := params.TrackingURL
 	trackingURL = strings.Replace(trackingURL, "{product_url}", params.ProductURL, 1)
-	encoded := base64.RawURLEncoding.EncodeToString([]byte(params.ClickID))
-	trackingURL = strings.Replace(trackingURL, "{click_id_base64}", encoded, 1)
+	encoded := base64.URLEncoding.EncodeToString([]byte(params.ClickID))
+	trackingURL = strings.Replace(trackingURL, "{click_id_base64}", strings.TrimRight(encoded, "="), 1)
 	return trackingURL
 }
