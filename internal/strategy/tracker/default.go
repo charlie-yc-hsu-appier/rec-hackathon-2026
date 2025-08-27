69911ca29882423a63d21bd2f89c58f38f76674f
Yvonne Feng <yvonne.feng@appier.com>
2025-08-27 14:31:16 +0800

    feat: setup standalone function for encoding click id

--- internal/strategy/tracker/default.go
+++ internal/strategy/tracker/default.go
@@ -10,7 +10,11 @@ type Default struct{}
 func (s *Default) GenerateTrackingURL(params Params) string {
 	trackingURL := params.TrackingURL
 	trackingURL = strings.Replace(trackingURL, "{product_url}", params.ProductURL, 1)
-	encoded := base64.URLEncoding.EncodeToString([]byte(params.ClickID))
-	trackingURL = strings.Replace(trackingURL, "{click_id_base64}", strings.TrimRight(encoded, "="), 1)
+	trackingURL = strings.Replace(trackingURL, "{click_id_base64}", encodeClickID(params.ClickID), 1)
 	return trackingURL
+}
+
+func encodeClickID(clickID string) string {
+	encoded := base64.URLEncoding.EncodeToString([]byte(clickID))
+	return strings.TrimRight(encoded, "=")
 }
