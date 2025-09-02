46695f851b8f0d14490910d86fe85eb25746863b
Yvonne Feng <yvonne.feng@appier.com>
2025-09-02 09:17:20 +0800

    feat: move shared EncodeClickID function to utils package

--- internal/strategy/tracker/default.go
+++ internal/strategy/tracker/default.go
@@ -1,8 +1,8 @@
 package tracker
 
 import (
-	"encoding/base64"
 	"net/url"
+	"rec-vendor-api/internal/strategy/utils"
 	"strings"
 )
 
@@ -12,12 +12,7 @@ func (s *Default) GenerateTrackingURL(params Params) string {
 	trackingURL := params.TrackingURL
 	trackingURL = strings.Replace(trackingURL, "{product_url}", params.ProductURL, 1)
 	trackingURL = strings.Replace(trackingURL, "{encoded_product_url}", url.QueryEscape(params.ProductURL), 1)
-	trackingURL = strings.Replace(trackingURL, "{click_id_base64}", encodeClickID(params.ClickID), 1)
+	trackingURL = strings.Replace(trackingURL, "{click_id_base64}", utils.EncodeClickID(params.ClickID), 1)
 	trackingURL = strings.Replace(trackingURL, "{user_id_lower}", strings.ToLower(params.UserID), 1)
 	return trackingURL
-}
-
-func encodeClickID(clickID string) string {
-	encoded := base64.URLEncoding.EncodeToString([]byte(clickID))
-	return strings.TrimRight(encoded, "=")
 }
