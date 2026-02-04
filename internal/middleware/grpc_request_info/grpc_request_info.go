e5726c3697af691cc5a7b98cc06f7f2181d4495b
Will Chen <will.wc.chen@appier.com>
2026-02-04 16:31:43 +0800

    refactor: functional programming-like revamp for request_info build up

--- internal/middleware/grpc_request_info/grpc_request_info.go
+++ internal/middleware/grpc_request_info/grpc_request_info.go
@@ -24,57 +24,64 @@ const (
 
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
-		ctx = setRequestInfo(ctx, info.FullMethod, req)
+		requestInfo := buildRequestInfo(ctx, info.FullMethod, req)
+		ctx = telemetry.RequestInfoToContext(ctx, requestInfo)
 		return handler(ctx, req)
 	}
 }
 
-func setRequestInfo(ctx context.Context, fullMethod string, req any) context.Context {
-	requestInfo := telemetry.RequestInfo{
+func buildRequestInfo(ctx context.Context, fullMethod string, req any) telemetry.RequestInfo {
+	traceID := extractTraceID(ctx)
+	siteID, oid, bidObjID, reqID := extractMetadataHeaders(ctx)
+	siteID = applyDefault(siteID, "unknown")
+	oid = applyDefault(oid, "unknown")
+	vendorKey, subID := extractRequestParams(req)
+
+	return telemetry.RequestInfo{
 		MethodName: getMethodName(fullMethod),
+		TraceID:    traceID,
+		SiteID:     siteID,
+		OID:        oid,
+		BidObjID:   bidObjID,
+		ReqID:      reqID,
+		VendorKey:  vendorKey,
+		SubID:      subID,
 	}
-
-	extractTraceID(ctx, &requestInfo)
-	extractMetadataHeaders(ctx, &requestInfo)
-	setDefaultMetadataValues(&requestInfo)
-	extractRequestParams(req, &requestInfo)
-
-	return telemetry.RequestInfoToContext(ctx, requestInfo)
 }
 
-func extractTraceID(ctx context.Context, info *telemetry.RequestInfo) {
+func extractTraceID(ctx context.Context) string {
 	spanCtx := trace.SpanContextFromContext(ctx)
 	if spanCtx.HasTraceID() && spanCtx.IsSampled() {
-		info.TraceID = spanCtx.TraceID().String()
+		return spanCtx.TraceID().String()
 	}
+	return ""
 }
 
-func extractMetadataHeaders(ctx context.Context, info *telemetry.RequestInfo) {
+func extractMetadataHeaders(ctx context.Context) (siteID, oid, bidObjID, reqID string) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return
+		return "", "", "", ""
 	}
 
-	info.SiteID = strings.Join(md.Get(HeaderSiteID), "")
-	info.OID = strings.Join(md.Get(HeaderOID), "")
-	info.BidObjID = strings.Join(md.Get(HeaderBidObjID), "")
-	info.ReqID = strings.Join(md.Get(HeaderReqID), "")
+	siteID = strings.Join(md.Get(HeaderSiteID), "")
+	oid = strings.Join(md.Get(HeaderOID), "")
+	bidObjID = strings.Join(md.Get(HeaderBidObjID), "")
+	reqID = strings.Join(md.Get(HeaderReqID), "")
+	return siteID, oid, bidObjID, reqID
 }
 
-func setDefaultMetadataValues(info *telemetry.RequestInfo) {
-	if info.SiteID == "" {
-		info.SiteID = "unknown"
+func applyDefault(value, defaultValue string) string {
+	if value == "" {
+		return defaultValue
 	}
-	if info.OID == "" {
-		info.OID = "unknown"
-	}
+	return value
 }
 
-func extractRequestParams(req any, info *telemetry.RequestInfo) {
+func extractRequestParams(req any) (vendorKey, subID string) {
 	if getRecommendationsReq, ok := req.(*schema.GetRecommendationsRequest); ok {
-		info.VendorKey = getRecommendationsReq.VendorKey
-		info.SubID = getRecommendationsReq.Subid
+		return getRecommendationsReq.VendorKey, getRecommendationsReq.Subid
 	}
+	return "", ""
 }
 
 func getMethodName(fullMethod string) string {
