66da839454ccb1729359d6e4a32f975dd6aa3ea0
Will Chen <will.wc.chen@appier.com>
2026-02-04 14:44:06 +0800

    refactor: leave used constants only

--- internal/constants/grpc_methods.go
+++ internal/constants/grpc_methods.go
@@ -2,11 +2,5 @@ package constants
 
 // proto generated package currently does not provide method name variables
 const (
-	MethodGetRecommendations = "GetRecommendations"
-	MethodGetVendors         = "GetVendors"
-	MethodHealthCheck        = "HealthCheck"
-
-	FullMethodGetRecommendations = "/vendorapi.VendorAPI/GetRecommendations"
-	FullMethodGetVendors         = "/vendorapi.VendorAPI/GetVendors"
-	FullMethodHealthCheck        = "/vendorapi.VendorAPI/HealthCheck"
+	FullMethodHealthCheck = "/vendorapi.VendorAPI/HealthCheck"
 )
